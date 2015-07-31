de95053804a6c72581d2795925994971eeffd7f9
ghost <kas_kon5@wp.pl>
2015-07-31 08:09:04 +0200

    added some tests to AstComparatorSpec

--- src/test/groovy/pl/mg6/utils/AstComparatorSpec.groovy
+++ src/test/groovy/pl/mg6/utils/AstComparatorSpec.groovy
@@ -32,4 +32,31 @@ class AstComparatorSpec extends Specification {
         def e = thrown(RuntimeException)
         e.message.startsWith "Node count different 3 != 2. \nActual: "
     }
+
+    def "Should fail on different methods names"() {
+        def test = new AstResourcesHelper().loadTest('/astcomparator/DifferentMethodName')
+        when:
+        assertThat test[0] isEqualTo test[1]
+        then:
+        def e = thrown(RuntimeException)
+        e.message.startsWith "Different method names. \nActual:"
+    }
+
+    def "Should fail on different methods arg types"() {
+        def test = new AstResourcesHelper().loadTest('/astcomparator/DifferentMethodArgType')
+        when:
+        assertThat test[0] isEqualTo test[1]
+        then:
+        def e = thrown(RuntimeException)
+        e.message.startsWith "Different method names. \nActual:"
+    }
+
+    def "Should fail on different methods bodies"() {
+        def test = new AstResourcesHelper().loadTest('/astcomparator/DifferentMethodBody')
+        when:
+        assertThat test[0] isEqualTo test[1]
+        then:
+        def e = thrown(RuntimeException)
+        e.message.startsWith "Different types of nodes. \nActual:"
+    }
 }
